0798429c1b22190079ca9bbc96314b5c8ac72b29
Aditya Kesuma <aditezo24@gmail.com>
2022-10-23 12:26:36 +0700

    Saya membuat  dan menambahkan menjadi 100

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$i = 0;
+
+$i += 100;
+
+?>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh</title>
+</head>
+
+<body>
+    <h1><?php $i ?></h1>
+</body>
+
+</html>
